4f171506acbb60a1dcde2961b7f56a3ab8584d9a
Lachlan Deck <lachlan.deck@gmail.com>
2011-09-28 10:10:17 +1000

    adding cua-mode as enabled

--- rails-setup.el
+++ rails-setup.el
@@ -47,6 +47,12 @@
 (global-hl-line-mode t)
 ;(highlight-current-line-set-bg-color "#E5F5B3")
 
+(require 'cua)
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t)
+
 
 ;;
 ;; files / history
