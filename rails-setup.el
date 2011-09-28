ede59fc34af00688d6ba44dc050fdd3dbe4de970
Lachlan Deck <lachlan.deck@gmail.com>
2011-09-28 11:05:56 +1000

    use M-RET for cua-set-rectangle-mark instead of C-RET which doesn't work in the terminal.

--- rails-setup.el
+++ rails-setup.el
@@ -47,12 +47,13 @@
 (global-hl-line-mode t)
 ;(highlight-current-line-set-bg-color "#E5F5B3")
 
+;(require 'cua)
 (cua-mode t)
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
-(transient-mark-mode 1) ;; No region when it is not highlighted
+;(transient-mark-mode 1) ;; No region when it is not highlighted
 (setq cua-keep-region-after-copy t)
-;(cua-selection-mode t) ; for standard emacs key-commands
-
+(cua-selection-mode t) ; for standard emacs key-commands
+(global-set-key (kbd "M-RET") 'cua-set-rectangle-mark) ; C-RET doesn't work in the terminal
 
 ;;
 ;; files / history
