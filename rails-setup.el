c8e67f652c10215ba230684937cf78ad24abbe84
Lachlan Deck <lachlan.deck@gmail.com>
2011-09-28 10:12:11 +1000

    fix cua config

--- rails-setup.el
+++ rails-setup.el
@@ -47,11 +47,11 @@
 (global-hl-line-mode t)
 ;(highlight-current-line-set-bg-color "#E5F5B3")
 
-(require 'cua)
 (cua-mode t)
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
 (setq cua-keep-region-after-copy t)
+;(cua-selection-mode t) ; for standard emacs key-commands
 
 
 ;;
