60919442eb047a73cfa7aec289500716465cfc4b
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-08 05:33:25 +1100

    [fix] no need to check if did hasn't changed for msk-ecb-after-directory-change-hook

--- lib/msk-shared-functions.el
+++ lib/msk-shared-functions.el
@@ -99,7 +99,7 @@
   (let ((diroldfull (expand-file-name (directory-file-name dirold)))
         (dirnewfull (expand-file-name (directory-file-name dirnew))))
     (if (run-hook-with-args-until-success 'msk-ecb-after-directory-change-toggle-p dirold dirnew)
-        (run-hook-with-args 'msk-ecb-after-directory-change-hook dirold dirnew)))))
+        (run-hook-with-args 'msk-ecb-after-directory-change-hook dirold dirnew))))
 
 (defun msk-ecb-activation-hook ()
   "hook for when ecb is activated"
