7069c797e92459b36c403eb801f3909aee8bbddf
Lachlan Deck <lachlan.deck@gmail.com>
2011-10-11 17:37:06 +1100

    copying el-get-install.el to a function and altering it so that if the branch chosen for el-get is 'master' then it doesn't bother switching branches.

--- lib/msk-el-get-autoloads.el
+++ lib/msk-el-get-autoloads.el
@@ -38,18 +38,89 @@
 
 (defconst msk-el-get-sources (expand-file-name (concat user-emacs-directory user-login-name "-starter-kit/package/msk-el-get-sources.el")))
 
+(defun clone-el-get ()
+  "I want el-get without switching to stable branch!
+   This is a basic copy of the el-get-install.el script
+   that will only branch from the master, not to it"
+  (progn
+    (let ((el-get-root
+           (file-name-as-directory
+            (or (bound-and-true-p el-get-dir)
+                (concat (file-name-as-directory user-emacs-directory) "el-get")))))
+
+      (when (file-directory-p el-get-root)
+        (add-to-list 'load-path el-get-root))
+
+      ;; try to require el-get, failure means we have to install it
+      (unless (require 'el-get nil t)
+        (unless (file-directory-p el-get-root)
+          (make-directory el-get-root t))
+
+        (let* ((package   "el-get")
+               (buf       (switch-to-buffer "*el-get bootstrap*"))
+               (pdir      (file-name-as-directory (concat el-get-root package)))
+               (git       (or (executable-find "git")
+                              (error "Unable to find `git'")))
+               (url       (or (bound-and-true-p el-get-git-install-url)
+                              "http://github.com/dimitri/el-get.git"))
+               (default-directory el-get-root)
+               (process-connection-type nil)   ; pipe, no pty (--no-progress)
+
+               ;; First clone el-get
+               (status
+                (call-process
+                 git nil `(,buf t) t "--no-pager" "clone" "-v" url package)))
+          
+          (unless (zerop status)
+            (error "Couldn't clone el-get from the Git repository: %s" url))
+          
+          ;; switch branch if we have to unless requiring `master'
+          (unless (and (boundp 'el-get-master-branch)
+                       (equal el-get-master-branch "master"))
+            (let* ((branch (cond
+                            ;; Check if a specific branch is requested
+                            ((bound-and-true-p el-get-install-branch))
+                            ;; Check if master branch is requested
+                            ((boundp 'el-get-master-branch) "master")
+                            ;; Read the default branch from the el-get recipe
+                            ((plist-get (with-temp-buffer
+                                          (insert-file-contents-literally
+                                           (expand-file-name "recipes/el-get.rcp" pdir))
+                                          (read (current-buffer)))
+                                        :branch))
+                            ;; As a last resort, use the master branch
+                            ("master")))
+                   (remote-branch (format "origin/%s" branch))
+                   (default-directory pdir)
+                   (bstatus
+                    (call-process git nil (list buf t) t "checkout" "-t" remote-branch)))
+              (unless (zerop bstatus)
+                (error "Couldn't `git checkout -t %s`" branch))))
+          
+          (add-to-list 'load-path pdir)
+          (load package)
+          (let ((el-get-default-process-sync t) ; force sync operations for installer
+                (el-get-verbose t))		    ; let's see it all
+            (el-get-post-install "el-get"))
+          (with-current-buffer buf
+            (goto-char (point-max))))))))
+
 (if (file-exists-p msk-el-get-sources)
   (progn 
     
     (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-    
+
+    ;; ;; auto-checkout current stable branch
+    ;; (unless (require 'el-get nil t)
+    ;;   (with-current-buffer
+    ;;       (url-retrieve-synchronously
+    ;;        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    ;;     (end-of-buffer)
+    ;;    (eval-print-last-sexp)))
+
     (unless (require 'el-get nil t)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-        (end-of-buffer)
-        (eval-print-last-sexp)))
-    
+      (clone-el-get))
+
     (require 'msk-el-get-sources)
     
     (if (boundp 'msk-el-get-packages)
@@ -58,6 +129,3 @@
           (el-get 'wait)
           ))
 ))
-
-
-
